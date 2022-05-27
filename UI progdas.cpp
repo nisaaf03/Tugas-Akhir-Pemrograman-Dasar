f9c1d856ebcc1bdc1624746c5d93d48a48b2829d
nisaaf03 <95396873+nisaaf03@users.noreply.github.com>
2022-05-27 13:08:06 +0700

    Add files via upload
    untuk program kalkulator sin dan akar kuadrat

---
+++ UI progdas.cpp
@@ -0,0 +1,20 @@
+
+// Online C++ Compiler - Build, Compile and Run your C++ programs online in your favorite browser
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int operasi;
+    cout<<"*************************************** \n";
+    cout<<"         SCIENTIFIC CALCULATOR \n";
+    cout<<"*************************************** \n";
+    cout<<"silahkan pilih operasi yang diinginkan: \n";
+    cout<<" 1. Addition (Penjumlahan) \n 2. Subtraction (Pengurangan) \n 3. Division (Pembagian) \n 4. Multiplication (Perkalian) \n 5. Sin \n 6. Cos \n 7. Tan \n 8. Eksponen \n 9. Square root (Akar kuadrat) \n 10. inverse sin \n 11. inverse cos \n 12. invers tan \n";
+    cout<<"________________________________________________________________ \n ";
+    cout<<"Ketik angka pada operasi yang diinginkan untuk melakukan operasi \n ";
+    cin>>operasi;
+    return 0;
+}
